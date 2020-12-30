c7c9c2c4ddf5a2c881382a5685c1a4a4658574c7
tonyg <aj.gianno@gmail.com>
2020-12-29 21:52:25 -0600

    bearer token

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const API_TOKEN = process.env.API_TOKEN;
 
 console.log(API_TOKEN);
 
-function handleGetTypes(_req, res, _next) {
+function handleGetTypes(req, res, next) {
   res.json([
     `Bug`,
     `Dark`,
@@ -33,7 +33,25 @@ function handleGetTypes(_req, res, _next) {
   ]);
 }
 
-app.get("/types", handleGetTypes);
+function validateBearerToken(req, res, next) {
+  const authVal = req.get("Authorization") || "";
+  // verify bearer token header
+  if (!authVal.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Missing Bearer token" });
+  }
+  //verify token matches our secret
+  // authVal === 'Bearer my-secret'
+
+  const token = authVal.split(" ")[1];
+
+  if (token !== API_TOKEN) {
+    return res.status(401).json({ message: "Invalid credential" });
+  }
+  //all validations passed move to next middleware in pipeline
+  next();
+}
+
+app.get("/types", validateBearerToken, handleGetTypes);
 app.get("/valid-types", handleGetTypes);
 
 app.listen(8080, () => {
